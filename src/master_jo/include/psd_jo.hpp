00991944f02b2194da69f206480de2c08e642eac
kmj2006 <mjkim8820@gmail.com>
2025-11-27 02:54:27 +0900

    UDP 통신 성공 코드

--- src/master_jo/include/psd_jo.hpp
+++ src/master_jo/include/psd_jo.hpp
@@ -2,6 +2,7 @@
 #define PSD_JO_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/u_int16_multi_array.hpp"
 
 class PsdJo : public rclcpp::Node
 {
@@ -9,4 +10,8 @@ public:
   PsdJo();
 
 private:
+  void topic_callback(const std_msgs::msg::UInt16MultiArray::SharedPtr msg) const;
+  rclcpp::Subscription<std_msgs::msg::UInt16MultiArray>::SharedPtr subscription_;
 };
+
+#endif
--- src/master_jo/src/psd_jo.cpp
+++ src/master_jo/src/psd_jo.cpp
@@ -1,16 +1,38 @@
 #include "psd_jo.hpp"
 
-PsdJo::PsdJo()
-: Node("psd_jo")
+using std::placeholders::_1;
+
+PsdJo::PsdJo() : Node("psd_jo")
 {
-  RCLCPP_INFO(this->get_logger(), "PsdJo Node start");
+  subscription_ = this->create_subscription<std_msgs::msg::UInt16MultiArray>(
+    "robit_psd",
+    10,
+    std::bind(&PsdJo::topic_callback, this, _1));
+    
+  RCLCPP_INFO(this->get_logger(), "psd subscribe start");
+}
+
+void PsdJo::topic_callback(const std_msgs::msg::UInt16MultiArray::SharedPtr msg) const
+{
+  uint16_t left = msg->data[0];
+  uint16_t front = msg->data[1];
+  uint16_t right = msg->data[2];
+
+  RCLCPP_INFO(this->get_logger(), 
+    "PSD 값 | 정면: %d | 왼쪽: %d | 오른쪽: %d", // 가까우면 값 커짐
+    left, front, right);
+
+  /*
+  if (front < 200) {
+      RCLCPP_WARN(this->get_logger(), "꺅 장애물이다!");
+  }
+  */
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<PsdJo>();
-  rclcpp::spin(node);
+  rclcpp::spin(std::make_shared<PsdJo>());
   rclcpp::shutdown();
   return 0;
 }
