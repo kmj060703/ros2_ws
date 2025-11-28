3006308b57f3f2f9eb45f56b4fb0f884f1def6e3
Song-yeonu <yeonu0070@kw.ac.kr>
2025-11-28 20:37:10 +0900

    Add files via upload

---
+++ src/ui_test/include/ui_test/controlitem.hpp
@@ -0,0 +1,11 @@
+#ifndef CONTROLITEM_HPP
+#define CONTROLITEM_HPP
+
+extern int state_flag_;
+extern int start_flag_;
+extern int forw_back_;
+extern int left_right_;
+extern double x_;
+extern double z_;
+
+#endif // CONTROLITEM_HPP
---
+++ src/ui_test/src/controlitem.cpp
@@ -0,0 +1,8 @@
+#include "../include/ui_test/controlitem.hpp"
+
+int state_flag_ = 0;
+int start_flag_ = 0;
+int forw_back_ = 0;
+int left_right_ = 0;
+double x_ = 0.0;
+double z_ = 0.0;
